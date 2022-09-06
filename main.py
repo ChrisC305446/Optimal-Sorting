a5fd2c17dde1c01142b3fba0039056fcbae72aa9
Evan Bowerman <evan.bowerman@student.allenisd.org>
2022-09-06 14:48:06 -0500

    pretty much everything but the sort algorithim itself

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+def sort(unfiltered):
+  filtered = unfiltered
+  
+  return filtered
+
+#########sort function
+
 import sys
 
 words = []
-for line in sys.stdin:
-  words.append(line.rstrip())
-  print(words)
-  #make words array from stdin
-  
+try:
+  for line in sys.stdin:
+    words.append(line.rstrip())
+    print(words)
+    #make words array from stdin
+except:
+  raise Exception(f"invalid input from stdin: {sys.stdin}")
+  #error catch
+###########turn stdin into array of strings
+
+
+output = sort(words)
+print(f"sorted array = {output}")
+###########output
