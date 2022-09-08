4f02d070a5f5d002040c25ee867bba3e91a00ed8
aclassypotater <bowermanevan@gmail.com>
2022-09-07 20:23:03 -0500

    improved readability

--- main.py
+++ main.py
@@ -5,24 +5,30 @@ def sort(unfiltered):
 
 #########sort function
 
+
 import sys
 
 words = []
 try:
-  for line in sys.stdin:
+  for line in sys.stdin:#make words array from stdin
+    
     if line.rstrip() == "!end":
       break # for ending stdin on vscode
+    
     words.append(line.rstrip().lower())
-    print(words)
-    #make words array from stdin
+   
+   
+    
 except:
   raise Exception(f"invalid input from stdin: {sys.stdin}")
   #error catch
+
 ###########turn stdin into array of strings
+
 
 
 output = sort(words)
 
 for word in output:
-  sys.stdout.write(f"{word}\n")
+  sys.stdout.write(f"{word}\n")#output as stdout 
 ###########output
