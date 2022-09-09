541fa1a92e6eb60bddffe83beeb68da1be1dcded
aclassypotater <bowermanevan@gmail.com>
2022-09-09 12:59:04 -0700

    removed leftover debug print

--- main.py
+++ main.py
@@ -68,7 +68,6 @@ def sort(unfiltered):
   str_index = 0
   for char in range(0,largest_string): #what character is being tested
     while is_sorted(filtered,str_index) == False: # while current character index isnt sorted
-      print(str(str_index) + " not sorted")
       if str_index == 0: # first character
         for index, word in enumerate(unfiltered):
           if unfiltered.__len__() != index+1: #if not at end of array
