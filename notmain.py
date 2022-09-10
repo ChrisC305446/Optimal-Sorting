99bba87eab8c2357aeb657a56a794c46c75523e2
Shariq Hasan <shariq.hasan@student.allenisd.org>
2022-09-09 23:23:52 -0500

    Adds other.py which is a more concise program than main.py

--- notmain.py
+++ notmain.py
@@ -3,9 +3,11 @@ import sys
 words = []
 
 for line in sys.stdin:
-        if 'q' == line.rstrip():
-                break
-        words.append(line)
-
+       if 'q' == line.rstrip():
+              break
+       
+       words.append(line)
+       
 words.sort()
 print(words)
+       
