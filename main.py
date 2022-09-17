226d459dd5ad7ca395d8a47ca45a98f148c51198
Shariq Hasan <shariq.hasan@student.allenisd.org>
2022-09-16 21:41:02 -0500

    fixed the "ussing integer operations for strings"

--- main.py
+++ main.py
@@ -9,36 +9,25 @@ for line in sys.stdin:
 
 words = [i.strip() for i in words]
        
-#sort begins here
-#anyChanges = True
-#while anyChanges == True:
-#       anyChanges = False
-#       forCount = 0
-#       for word in range(len(words)-1):
-#              word2 = word+1
-#              if words[word].lower() > words[word2].lower():
-#                     words[word], words[word2] = words[word2], words[word]
-#                     anyChanges = True
+def partition(l, r, nums):
+       pivot, ptr = words[r], l
+       for i in range(l, r):
+              if words[i] <= pivot:
+                     words[i], words[ptr] = words[ptr], words[i]
+                     ptr += 1
+       nums[ptr], nums[r] = nums[r], nums[ptr]
+       return ptr
 
-def split(left, right, words):
-       last, first = right, left
-       for i in range(left, right):
-              if words[i] <= first:
-                     words[i], words[first] = words[first], words[i]
-                     first += 1
-              nums[first], nums[right] = nums[right], nums[first]
-              return first
-
-def quicksort(left, right, words):
+def quicksort(l, r, words):
        if len(words) == 1:
               return words
-       if left < right:
-              mid = split(left, right, words)
-              quicksort(left, mid-1, words)
-              quicksort(mid+1, right, words)
+       if l < r:
+              pi = partition(l, r, words)
+              quicksort(l, pi-1, words)
+              quicksort(pi+1, r, words)
        return words
 
-words = quicksort(words[0], words[len(words)-1], words)
-              
+words = quicksort(0, len(words)-1, words)
+
 for word in words:
-  sys.stdout.write(f"{word}\n")
+       sys.stdout.write(f"{word}\n")
